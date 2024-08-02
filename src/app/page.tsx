3cf653bb66ff3ad2c230213fc0f9a458f455d62a
VICTOR <95864890+Legolixx@users.noreply.github.com>
2024-08-02 16:36:43 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,13 @@
 
+import Image from "next/image";
+import Header from "./__components/Nav";
+import Hero from "./__components/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+}
